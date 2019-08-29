e4b4101cb8fd0be75e01fd1a56d162c2dde83809
Jordan Hipwell <dont@me>
2019-08-29 13:31:53 -0600

    Initial Commit

---
+++ SamplePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SamplePlayer
+//
+//  Created by Jordan Hipwell on 8/29/19.
+//  Copyright © 2019 Jordan Hipwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
