19bd491a29dbd77c4452741a82ed5edd57e9d628
Jordan Hipwell <dont@me>
2019-12-23 15:01:24 -0700

    Use AKMixer

--- SamplePlayer/Maestro.swift
+++ SamplePlayer/Maestro.swift
@@ -13,50 +13,61 @@ final class Maestro: NSObject {
     
     static let shared = Maestro()
     
-    var trackPlayers = [AKPlayer]()
+    private var trackPlayers = [AKPlayer]() {
+        didSet {
+            do {
+                try AudioKit.stop()
+            } catch {
+                print("Maestro AudioKit.stop error: \(error)")
+            }
+            
+            mixer = AKMixer(trackPlayers)
+            AudioKit.output = mixer
+            
+            do {
+                try AudioKit.start()
+            } catch {
+                print("Maestro AudioKit.start error: \(error)")
+            }
+            
+            trackPlayers.forEach {
+                if $0.isPlaying {
+                    let pos = $0.currentTime
+                    $0.stop()
+                    $0.play(from: pos)
+                }
+            }
+        }
+    }
+    private var mixer: AKMixer?
     
-    let trackURLs = [
-        Bundle.main.url(forResource: "SampleAudio_0.7mb", withExtension: "mp3")!,
-        Bundle.main.url(forResource: "SampleAudio_0.4mb", withExtension: "mp3")!
+    private let trackURLs = [
+        Bundle.main.url(forResource: "SampleAudio_0.4mb", withExtension: "mp3")!,
+        Bundle.main.url(forResource: "SampleAudio_0.7mb", withExtension: "mp3")!
     ]
     
     func playFirstTrack() {
         playNewPlayer(fileURL: trackURLs[0])
     }
     
-    func playNewPlayer(fileURL: URL) {
-        let newPlayer = AKPlayer(url: fileURL)!
-        trackPlayers.append(newPlayer)
-        
-        AudioKit.output = newPlayer //boom
-        
-        do {
-            try AudioKit.start()
-            newPlayer.play()
-        } catch {
-            print("Maestro AudioKit.start error: \(error)")
-        }
-    }
-    
     func next() {
         trackPlayers.forEach { $0.stop() }
         trackPlayers.removeAll()
-
-        do {
-            try AudioKit.stop()
-        } catch {
-            print("Maestro AudioKit.stop error: \(error)")
-        }
         
         playNewPlayer(fileURL: trackURLs[1])
     }
     
     func fadeAndStartNext() {
-        //TODO: This crashes because we change the output without stopping AudioKit
-        //but we can't stop playback of the first AKPlayer
-        //Note that the fading logic is not included here
-        
         playNewPlayer(fileURL: trackURLs[1])
+        
+        //here we would adjust the volume of the players and remove the first player after 3 seconds
+    }
+    
+    private func playNewPlayer(fileURL: URL) {
+        let newPlayer = AKPlayer(url: fileURL)!
+        trackPlayers.append(newPlayer) //triggers didSet to update AudioKit.output
+        
+        newPlayer.play()
     }
     
 }
